2aec11ddf9a0dc97b437aa73231d62c1c2556beb
shevchukmaxim <webmaxsup@gmail.com>
2018-06-04 23:58:45 +0300

    Что-то заработало

--- src/script/aceframe.js
+++ src/script/aceframe.js
@@ -1,17 +1,33 @@
 $(document).ready(function(){
-
 	var editor = ace.edit("editor");
 	editor.session.setMode("ace/mode/html");
 	// enable emmet on the current editor
 	editor.setOption("enableEmmet", true);
 
-	var iframe =  document.getElementById('myframe');
-	iframeBody = iframe.contentWindow.document.getElementsByTagName('html')[0];
 
-	var framecontent = document.getElementById('myframe').contentDocument.getElementsByTagName('html')[0];
-	editor.setValue(framecontent.innerHTML, 1);
+	var iframe =  document.getElementById('preview');
+	//var iframeBody = iframe.getElementsByTagName('html');
+	
+var contentz ='\
+<style type="text/css">\n\
+	.dot {\n\
+	  height: 25px;\n\
+	  width: 25px;\n\
+	  background-color: #FFFF00;\n\
+	  display: inline-block;\n\
+	}\n\
+</style>\n\n\
+<div class="dot"></div>\n\
+<div class="dot"></div>\n\
+<div class="dot"></div>\n\
+<div class="dot"></div>\n\
+';
+	iframe.innerHTML = contentz;
+	
+	var framecontent = iframe.innerHTML;
+	editor.setValue(framecontent, 1);
 	editor.getSession().on('change', function(e) {
 		var x = editor.getValue();
-		iframeBody.innerHTML = x;
+		iframe.innerHTML = x;
 	});
 });
